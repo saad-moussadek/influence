d5c251aadffdb0d7faac18f3dc81ce12619b9e6e
Saad Moussadek <sm398@st-andrews.ac.uk>
2018-07-27 15:42:15 +0100

    Modular side bar tabs - cards structure - starting card drafts

---
+++ front_end/src/components/DrawerTabs.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import PropTypes from 'prop-types';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import StarIcon from '@material-ui/icons/Star';
+import SendIcon from '@material-ui/icons/Send';
+import MailIcon from '@material-ui/icons/Mail';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ReportIcon from '@material-ui/icons/Report';
+import routes from "../config/routes";
+import {NavLink} from "react-router-dom";
+import Divider from "@material-ui/core/es/Divider/Divider";
+import {withStyles} from "@material-ui/core/styles/index";
+
+
+const styles = theme => ({
+    toolbar: theme.mixins.toolbar,
+});
+
+const {classes, theme} = this.props;
+
+class DrawerTabs extends React.Component {
+    render() {
+        return (
+            <div className={classes.toolbar}>
+                {routes.map((prop, key) => {
+                    let fontSize = 10;
+                    if (!prop.redirect && !prop.separator) {
+                        return (
+                            <NavLink to={prop.path} className={'nav-link'}>
+
+                                <ListItem button>
+                                    <ListItemIcon>
+                                        {prop.icon()}
+                                    </ListItemIcon>
+                                    <ListItemText primaryTypographyProps={fontSize} primary={prop.name}/>
+                                </ListItem>
+
+                            </NavLink>
+                        );
+                    }
+                    else return null;
+                })}
+            </div>
+        )
+    }
+}
+
+DrawerTabs.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(DrawerTabs);
