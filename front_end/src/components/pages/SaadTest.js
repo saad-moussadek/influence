7add20b814462ecc4028da17c5348b313782108b
Secara <isecara@LONL160b9700e.local>
2018-08-01 23:34:19 +0100

    Added Testing Tabs

---
+++ front_end/src/components/pages/SaadTest.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MuiThemeProvider from "@material-ui/core/es/styles/MuiThemeProvider";
+import Typography from "@material-ui/core/es/Typography/Typography";
+import Router from "react-router";
+
+class SaadTest extends Component {
+    render() {
+        return (
+            <div>
+                Sniggerz
+            </div>
+        );
+    }
+}
+
+
+export default SaadTest;
