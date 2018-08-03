16976b3c085154f88338a78187fa2f9036662b0a
Saad Moussadek <sm398@st-andrews.ac.uk>
2018-08-03 23:32:29 +0100

    Added bar/line choice

--- front_end/src/components/diagrams/BasicChart.js
+++ front_end/src/components/diagrams/BasicChart.js
@@ -13,22 +13,93 @@ import Legend from "recharts/es6/component/Legend";
 import PropTypes from "prop-types";
 import {withStyles} from "@material-ui/core/styles/index";
 import withTheme from "@material-ui/core/es/styles/withTheme";
+import BarChart from "recharts/es6/chart/BarChart";
+import Bar from "recharts/es6/cartesian/Bar";
+import config from "../../config/config";
+import IconButton from "@material-ui/core/es/IconButton/IconButton";
+import ShowChart from "@material-ui/icons/es/ShowChart";
+import BarChartIcon from "@material-ui/icons/BarChart";
+
+const styles = theme => ({
+    button: {
+        marginTop: 0,
+        marginLeft: theme.spacing.unit,
+        display: "flex",
+        position: "absolute",
+        zIndex: 10,
+    },
+});
 
 
 class BasicChart extends React.Component {
-    render () {
-        let {data, graph, graph2, color, color2} = this.props;
+    state = {
+        lineChartVisibility: "visible",
+        lineChartShrink: 200,
+
+        barChartVisibility: "hidden",
+        barChartShrink: 0,
+    };
+
+    onClickSwitch = () => {
+        if (this.state.lineChartVisibility == "visible") {
+            this.setState({
+                lineChartVisibility: "hidden",
+                lineChartShrink: 0,
+
+                barChartVisibility: "visible",
+                barChartShrink: 200,
+            })
+        }
+        else {
+            this.setState({
+                lineChartVisibility: "visible",
+                lineChartShrink: 200,
+
+                barChartVisibility: "hidden",
+                barChartShrink: 0,
+            })
+        }
+    };
+
+    render() {
+        let {data, graph, graph2, color, color2, classes} = this.props;
+        if (color == null) color = config.theme.palette.primary[500];
+        if (color2 == null) color2 = config.theme.palette.secondary[500];
+        let secondBar;
+        if (graph2 != null)
+            secondBar = <Bar dataKey={graph2} fill={color2} strokeWidth={2}/>;
         return (
-            <ResponsiveContainer width={"100%"} height={200}>
-                <LineChart width={600} height={300} data={data}
-                           margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-                    <XAxis dataKey="date"/>
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <Tooltip/>
-                    <Line type="monotone" dataKey={graph} stroke={color} strokeWidth={2.5} />
-                    <Line type="monotone" dataKey={graph2} stroke={color2} strokeWidth={2} />
-                </LineChart>
-            </ResponsiveContainer>
+            <div>
+                <IconButton className={classes.button} aria-label="Delete" onClick={this.onClickSwitch}>
+                    <ShowChart style={{visibility: this.state.lineChartVisibility}}/>
+                </IconButton>
+                <IconButton className={classes.button} aria-label="Delete" onClick={this.onClickSwitch}>
+                    <BarChartIcon style={{visibility: this.state.barChartVisibility}}/>
+                </IconButton>
+                <ResponsiveContainer width={"100%"} height={this.state.lineChartShrink} style={{visibility: this.state.lineChartVisibility}}>
+                    <LineChart width={600} height={300} data={data}
+                               margin={{top: 5, right: 30, left: 20, bottom: 5}} style={{visibility: this.state.lineChartVisibility}}>
+                        <XAxis dataKey="date"/>
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <Tooltip/>
+                        <Line type="monotone" dataKey={graph} stroke={color} strokeWidth={2.5}/>
+                        <Line type="monotone" dataKey={graph2} stroke={color2} strokeWidth={2}/>
+                    </LineChart>
+                </ResponsiveContainer>
+                <ResponsiveContainer width={"100%"} height={this.state.barChartShrink} style={{visibility: this.state.barChartVisibility}}>
+                    <BarChart width={600} height={300} data={data}
+                              margin={{top: 5, right: 30, left: 20, bottom: 5}} style={{visibility: this.state.barChartVisibility}}>
+                        <XAxis dataKey="date"/>
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <Tooltip/>
+                        <Bar dataKey={graph} fill={color} strokeWidth={2.5}/>
+                        {
+                            secondBar
+                        }
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+
         );
     }
 }
@@ -41,5 +112,5 @@ BasicChart.propTypes = {
     color: PropTypes.object.isRequired,
 };
 
-export default withTheme()(BasicChart);
+export default withStyles(styles)(BasicChart);
 
